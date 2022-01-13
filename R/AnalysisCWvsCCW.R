b1d76b6cc619ec4324fe67e9fbaf7e3994683bc3
Jenn Ruttle <jennruttle@gmail.com>
2022-01-13 18:23:15 -0500

    Changed how many trials were included in t-test

--- R/AnalysisCWvsCCW.R
+++ R/AnalysisCWvsCCW.R
@@ -47,10 +47,15 @@ RM<-rbind(CWRM,CCWRM)
 
 print(sprintf("Comparison of CW and CCW localization targets for %s feedback", exp))
 print(t.test(RM$Aligned[RM$Direction == "CW"],RM$Aligned[RM$Direction == "CCW"]))
+print(etaSquaredTtest(RM$Aligned[RM$Direction == "CW"],RM$Aligned[RM$Direction == "CCW"], na.rm = TRUE))
 print(t.test(RM$R1_Early[RM$Direction == "CW"],RM$R1_Early[RM$Direction == "CCW"]))
+print(etaSquaredTtest(RM$R1_Early[RM$Direction == "CW"],RM$R1_Early[RM$Direction == "CCW"], na.rm = TRUE))
 print(t.test(RM$R1_Late[RM$Direction == "CW"],RM$R1_Late[RM$Direction == "CCW"]))
+print(etaSquaredTtest(RM$R1_Late[RM$Direction == "CW"],RM$R1_Late[RM$Direction == "CCW"], na.rm = TRUE))
 print(t.test(RM$R2[RM$Direction == "CW"],RM$R2[RM$Direction == "CCW"]))
+print(etaSquaredTtest(RM$R2[RM$Direction == "CW"],RM$R2[RM$Direction == "CCW"], na.rm = TRUE))
 print(t.test(RM$EC[RM$Direction == "CW"],RM$EC[RM$Direction == "CCW"]))
+print(etaSquaredTtest(RM$EC[RM$Direction == "CW"],RM$EC[RM$Direction == "CCW"], na.rm = TRUE))
 }
 
 
@@ -60,12 +65,12 @@ TUnbaselinedCombine<- function(data) {
   ParticipantRM<- data.frame()
   participants <- c(2:ncol(data))
   for (participant in participants){
-    Aligned<- mean(unlist(data[57:64,participant]), na.rm = TRUE)
+    Aligned<- mean(unlist(data[33:64,participant]), na.rm = TRUE)
     R1_Early<- mean(unlist(data[65:72,participant]), na.rm = TRUE)
     R1_Late<- mean(unlist(data[217:224,participant]), na.rm = TRUE)
     R2<- mean(unlist(data[233:240,participant]), na.rm = TRUE)
-    EC<- mean(unlist(data[241:248,participant]), na.rm = TRUE)
-    EC_Late<- mean(unlist(data[281:288,participant]), na.rm = TRUE)
+    EC<- mean(unlist(data[241:256,participant]), na.rm = TRUE)
+    EC_Late<- mean(unlist(data[257:288,participant]), na.rm = TRUE)
     RM<- data.frame(Aligned,R1_Early, R1_Late, R2, EC, EC_Late)
     if (prod(dim(ParticipantRM)) == 0) {
       ParticipantRM <- RM
